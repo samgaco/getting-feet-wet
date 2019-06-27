8d796389764e3c5e43f2b2d80965ef68f84d3d33
addo <forison6205@gmail.com>
2019-06-27 06:59:19 -0400

    added new users;microposts;index;new

--- toy_app/config/routes.rb
+++ toy_app/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'users/edit'
+  # get 'user/index'
+  # get 'user/show'
+  # get 'user/new'
+  # get 'user/edit'
   root 'application#hello'
 end
