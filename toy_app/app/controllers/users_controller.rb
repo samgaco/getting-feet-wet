eaabc3c7eb9f24acd2da63abeeb1ae3e527c8e29
Samuel <samuelgarciacompanys@gmail.com>
2019-06-27 16:00:23 +0200

    added users database update, link to the users id page and names on the index, problems: links to direct us to the index or to the edit page, prompt us to an error page

--- toy_app/app/controllers/users_controller.rb
+++ toy_app/app/controllers/users_controller.rb
@@ -1,15 +1,28 @@
 class UsersController < ApplicationController
+  include UsersHelper
+
   def index
     @users = User.all
   end
+  
+  def create 
+    @user = User.new(users_params)
+    @user.save 
+    redirect_to user_path(@user)
+  end 
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
-    @user = User.new 
+    @user = User.new
   end
 
   def edit
   end
+
+  def update 
+  end
+
 end
--- toy_app/config/routes.rb
+++ toy_app/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :users 
+
   get 'users/index'
   get 'users/new'
+  post 'users/new'
+  patch 'users/new'
   get 'users/show'
   get 'users/edit'
   # get 'user/index'
