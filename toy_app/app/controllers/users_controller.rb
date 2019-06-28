f1000585cce5d23c7a206048b41f3968236865a1
addo <forison6205@gmail.com>
2019-06-28 05:32:00 -0400

    crud refactory

--- toy_app/app/controllers/users_controller.rb
+++ toy_app/app/controllers/users_controller.rb
@@ -4,25 +4,31 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
-  def create 
-    @user = User.new(users_params)
-    @user.save 
-    redirect_to user_path(@user)
-  end 
 
-  def show
+  def new 
+    @new_users = User.new
+  end
+
+  def create
+    @new_user = User.new(users_params)
+    @new_user.save
+    flash.notice = "user '#{@new_user.name}' added!"
+    redirect_to User.find(@new_user.id)
+  end 
+  
+  def show 
     @user = User.find(params[:id])
   end
 
-  def new
-    @user = User.new
-  end
-
   def edit
+    @edit_user = User.find_by(params[:id])
   end
-
-  def update 
+  
+  def update
+    @user = User.find_by(params[:id])
+    @user.update(users_params)
+    flash.notice = "user '#{@user.name}' Updated!"
+    redirect_to user_path
   end
 
 end
--- toy_app/config/routes.rb
+++ toy_app/config/routes.rb
@@ -1,16 +1,3 @@
 Rails.application.routes.draw do
-
   resources :users 
-
-  get 'users/index'
-  get 'users/new'
-  post 'users/new'
-  patch 'users/new'
-  get 'users/show'
-  get 'users/edit'
-  # get 'user/index'
-  # get 'user/show'
-  # get 'user/new'
-  # get 'user/edit'
-  root 'application#hello'
 end
